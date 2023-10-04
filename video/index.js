48cc960ad5a70c1a08b3c13e2d8304bc0023b2fc
w-t-w <15866369958@163.com>
2023-10-04 13:01:52 +0800

    accumulation: think of nodejs

---
+++ video/index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const {graphqlHTTP} = require('koa-graphql');
+
+const schema = require('./lib');
+
+const PORT = 7777;
+
+const koa = new Koa();
+
+koa.use(graphqlHTTP({
+    schema
+}));
+
+koa.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:${PORT}!`);
+});
---
+++ video/lib/index.js
@@ -0,0 +1,20 @@
+const {buildSchema} = require('graphql');
+const fs = require('fs');
+const path = require('path');
+
+const comments = require('../data');
+
+const GRAPHQL_DIR = path.resolve(__dirname, '../gql/comments.gql');
+
+const schema = buildSchema(fs.readFileSync(GRAPHQL_DIR, 'utf-8'));
+
+schema.getQueryType().getFields().comments.resolve = () => {
+    return comments;
+};
+
+schema.getMutationType().getFields().praise.resolve = (argv0, {id}) => {
+    const comment = comments.find(item => item.id === id);
+    return comment && comment['praiseNum']++;
+};
+
+module.exports = schema;
